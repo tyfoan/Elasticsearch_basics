6dcc5a7f19ddc32d2a99f02b0ccf48495cda6669
Alexander <alex.vostrugo@gmail.com>
2016-12-22 15:43:30 +0200

    Searching method fixed

--- model/base_model.py
+++ model/base_model.py
@@ -15,11 +15,11 @@ class BaseModel:
         """
         self.id = search_result_item['_id']
         source = search_result_item['_source']
-        highlight = search_result_item['highlight']
+        highlight = search_result_item['highlight'] if 'highlight' in search_result_item else None
         for key in source.keys():
             if hasattr(self, key):
                 source_value = source[key]
-                highlight_value = highlight.get(key)
+                highlight_value = highlight.get(key) if highlight else None
                 value = highlight_value[0] if highlight_value else source_value
                 setattr(self, key, value)
         return self
